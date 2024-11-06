18f6adac07edff0c4965c3274fbd8ff4c0e31277
Dongmin <xua744531854@163.com>
2024-11-06 17:25:32 +0800

    [Auto] 2024-11-06下午17:25:32

---
+++ dist/static/js/bar~540a9b7a.f560adfe.js
@@ -0,0 +1,16 @@
+"use strict";
+(self['webpackChunkrspack_build_error_export'] = self['webpackChunkrspack_build_error_export'] || []).push([["129"], {
+"215": (function (__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__) {
+/* ESM import */var _source__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(706);
+
+console.log({
+    functionA: _source__WEBPACK_IMPORTED_MODULE_0__/* .functionA */.Ag,
+    functionB: _source__WEBPACK_IMPORTED_MODULE_0__/* .functionB */.mX,
+    functionC: _source__WEBPACK_IMPORTED_MODULE_0__/* .functionC */.E4,
+    functionD: _source__WEBPACK_IMPORTED_MODULE_0__/* .functionD */.e5
+});
+
+
+}),
+
+}]);
---
+++ dist/static/js/foo~0.a5ca7bb6.js
@@ -0,0 +1,13 @@
+"use strict";
+(self['webpackChunkrspack_build_error_export'] = self['webpackChunkrspack_build_error_export'] || []).push([["273"], {
+"50": (function (__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__) {
+/* ESM import */var _source__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(706);
+
+console.log({
+    functionA: _source__WEBPACK_IMPORTED_MODULE_0__/* .functionA */.Ag
+});
+
+
+}),
+
+}]);
